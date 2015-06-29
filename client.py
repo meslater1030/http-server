7bca68d61c4b9c08d8a034b91d508859838d8988
Megan Slater <megan.e.slater@gmail.com>
2015-06-29 15:04:02 -0700

    initial commit from class

---
+++ client.py
@@ -0,0 +1,22 @@
+import socket
+
+ADDR = ('127.0.0.1', 8000)
+client = socket.socket(
+    socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_IP
+    )
+
+client.connect(ADDR)
+
+msg = "do you hear me?"
+
+try:
+    client.sendall(msg)
+    while True:
+        part = client.recv(16)
+        client.shutdown(socket.SHUT_WR)
+        print part
+        if len(part) < 16:
+            client.close()
+            break
+except Exception as e:
+    print e

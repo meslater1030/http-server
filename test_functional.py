b600225c4e64de3189e80c986f7d54e8eaaf0d8f
Megan Slater <megan.e.slater@gmail.com>
2015-06-29 19:44:20 -0700

    add 500 test

--- test_functional.py
+++ test_functional.py
@@ -3,7 +3,6 @@
 
 import pytest
 import socket
-<<<<<<< Updated upstream
 #import server
 from multiprocessing import Process
 #from server import response_ok
@@ -55,45 +54,15 @@ def test_response_error():
 
 @pytest.fixture()
 def client_code():
-=======
-import sys
-# import server
-# # from server import response_ok
-# from multiprocessing import Process
+    ADDR = ('127.0.0.1', 8000)
+    client_conn = socket.socket(
+        socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_IP
+    )
 
+    client_conn.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-@pytest.yield_fixture
-def server_process():
-    process = Process(target=server.start_server)
-    process.daemon = True
-    process.start()
-    yield process
-
-# @pytest.fixture(scope="module")
-# def test_run_server(request):
-#     ADDR = ('127.0.0.1', 8000)
-#     socket_conn = socket.socket(
-#         socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_IP
-#     )
-
-#     socket_conn.bind(ADDR)
-
-#     socket_conn.listen(1)
-
-#     while True:
-#         try:
-#             conn, addr = socket_conn.accept()
-#             output = ""
-#             while True:
-#                 msg = conn.recv(16)
-#                 output = output + msg
-#                 if len(msg) < 16:
-#                     response_ok()
-#                     conn.sendall(output)
-#                     conn.close()
-#                     break
-#         except KeyboardInterrupt:
-#             break
+    client_conn.connect(ADDR)
+    return client_conn
 
 
 def test_function(client_code):
@@ -111,14 +80,27 @@ def test_function(client_code):
                 client_code.close()
                 break
         assert '<html>' in output
+        assert '200' in output
+        assert 'do you hear me at all?' in output
     except Exception as e:
         print e
-<<<<<<< Updated upstream
-=======
-    assert output[-15:] == msg
-    assert output[:15] == b"HTTP/1.1 200 OK"
 
-# this should be run while no server is running
-def test_response_error():
-    
->>>>>>> Stashed changes
+
+def test_500_function():
+    msg = "do you hear me at all?"
+
+    try:
+        client_code.sendall(msg)
+        output = ""
+        while True:
+            part = client_code.recv(16)
+            assert part
+            output = output + part
+            if len(part) < 16:
+                client_code.shutdown(socket.SHUT_WR)
+                client_code.close()
+                break
+        assert '<html>' in output
+        assert '500' in output
+    except Exception as e:
+        print e

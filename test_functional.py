0f1e2bb573dda2cfd5f91a96ec35911f96cc0257
Jesse Klein <jesse@Jesses-MacBook-Air.local>
2015-06-29 19:35:30 -0700

    modify test
    modify 'tes_functional.py'

--- test_functional.py
+++ test_functional.py
@@ -1,7 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-# import pytest
+import pytest
 import socket
-<<<<<<< HEAD
 #import server
 from multiprocessing import Process
 #from server import response_ok
@@ -16,33 +17,9 @@ def run_server():
     socket_conn.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
     socket_conn.bind(ADDR)
-=======
-import sys
-# import server
-# # from server import response_ok
-# from multiprocessing import Process
 
+    socket_conn.listen(1)
 
-# @pytest.yield_fixture
-# def server_process():
-#     process = Process(target=server.start_server)
-#     process.daemon = True
-#     process.start()
-#     yield process
-
-# @pytest.fixture(scope="module")
-# def test_run_server(request):
-#     ADDR = ('127.0.0.1', 8000)
-#     socket_conn = socket.socket(
-#         socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_IP
-#     )
-
-#     socket_conn.bind(ADDR)
->>>>>>> a79b0f0b7d747246cd2955f62857b1dfd154d938
-
-#     socket_conn.listen(1)
-
-<<<<<<< HEAD
     while True:
         try:
             conn, addr = socket_conn.accept()
@@ -57,22 +34,6 @@ import sys
                     break
         except KeyboardInterrupt:
             break
-=======
-#     while True:
-#         try:
-#             conn, addr = socket_conn.accept()
-#             output = ""
-#             while True:
-#                 msg = conn.recv(16)
-#                 output = output + msg
-#                 if len(msg) < 16:
-#                     response_ok()
-#                     conn.sendall(output)
-#                     conn.close()
-#                     break
-#         except KeyboardInterrupt:
-#             break
->>>>>>> a79b0f0b7d747246cd2955f62857b1dfd154d938
 
 
 @pytest.yield_fixture()
@@ -104,18 +65,13 @@ def client_code():
     return client_conn
 
 
-<<<<<<< HEAD
 def test_function(client_code):
     msg = "do you hear me at all?"
-=======
-    msg = "do you hear me?"
->>>>>>> a79b0f0b7d747246cd2955f62857b1dfd154d938
 
     try:
         client_code.sendall(msg)
         output = ""
         while True:
-<<<<<<< HEAD
             part = client_code.recv(16)
             assert part
             output = output + part
@@ -124,15 +80,5 @@ def test_function(client_code):
                 client_code.close()
                 break
         assert '<html>' in output
-=======
-            part = client_conn.recv(16)
-            output = output + part
-            if len(part) < 16:
-                sys.stdout.write(output)
-                client_conn.shutdown(socket.SHUT_WR)
-                client_conn.close()
-                break
->>>>>>> a79b0f0b7d747246cd2955f62857b1dfd154d938
     except Exception as e:
         print e
-    assert output[-15:] == msg

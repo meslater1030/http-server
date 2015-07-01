c579fb0aac0e07de104cc565ad3eaeb69809c89a
Megan Slater <megan.e.slater@gmail.com>
2015-06-30 19:15:05 -0700

    fixed merge

--- client.py
+++ client.py
@@ -7,23 +7,6 @@ client = socket.socket(
     )
 client.connect(ADDR)
 
-<<<<<<< HEAD
-msg = "do you hear me at all?"
-
-try:
-    client.sendall(msg)
-    output = ""
-    while True:
-        part = client.recv(16)
-        output = output + part
-        client.shutdown(socket.SHUT_WR)
-        if len(part) < 16:
-            print output
-            client.close()
-            break
-except Exception as e:
-    print e
-=======
 
 def send_request(msg):
     try:
@@ -42,4 +25,3 @@ def send_request(msg):
 
 if __name__ == "__main__":
     send_request("do you hear me?")
->>>>>>> 7f8194aa87327d7cdcbf416a9bf612cf37666d32

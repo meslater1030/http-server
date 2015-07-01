95c13df0a22891e7ff5f38a93dbd9fec405fe184
Megan Slater <megan.e.slater@gmail.com>
2015-06-30 18:31:15 -0700

    added parse function

--- server.py
+++ server.py
@@ -8,12 +8,15 @@ socket.bind(ADDR)
 socket.listen(2)
 
 
-def response_ok():
-    return "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n"
+def response_ok(reason_phrase):
+    return "HTTP/1.1 200" + reason_phrase + "\r\n"
+    "Content-Type: text/html; charset=utf-8\r\n"
 
 
-def response_error():
-    return "HTTP/1.1 500 not OK\r\nContent-Type: text/html; charset=utf-8\r\n"
+def response_error(error_code, reason_phrase):
+    response = "HTTP/1.1 " + error_code + " " + reason_phrase + "\r\n"
+    "Content-Type: text/html; charset=utf-8\r\n"
+    return response
 
 
 def return_request():
@@ -26,14 +29,29 @@ def return_request():
                 output = output + msg
                 if len(msg) < 16:
                     break
-            if "GET" in output:
-                conn.sendall(response_ok())
-                conn.sendall(output)
-            else:
-                conn.sendall(response_error())
+            try:
+                final = parse_request(output)
+                conn.sendall(response_ok(final))
+            except ValueError as detail:
+                detail = str(detail)
+                conn.sendall(response_error("400", detail))
             conn.close()
         except KeyboardInterrupt:
             break
+
+
+def parse_request(request):
+    if "GET" not in request:
+        raise ValueError("Error.  Must send GET request")
+    if "HTTP/1.1" not in request:
+        raise ValueError("Error. Only accepts HTTP 1.1 Protocol")
+    if "Host:" not in request:
+        raise ValueError("Error. Must provide host")
+    else:
+        request_lines = request.split(b"\r\n")
+        first_line = request_lines[0].split(" ")
+        return first_line[1]
+
 
 if __name__ == "__main__":
     return_request()
